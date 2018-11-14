83447403461e857cc67c90d8d3a0b510ac2bc769
Halalai Mircea <boss.mircea67@yahoo.com>
2018-11-14 19:12:23 +0200

    Implemented public methods which return extended netmask and network address.

--- IPcalc/IPcalc.cpp
+++ IPcalc/IPcalc.cpp
@@ -1,20 +1,32 @@
 #include "IPcalc.h"
-#include<cmath>
 
 
 IPcalc::IPcalc(std::string IPaddress)
 {
 	m_addressRaw = IPaddress;
-	m_longNetmask.fill(0);
-	m_network.fill(0);
-	m_broadcast.fill(0);
-	m_hostMin.fill(0);
-	m_hostMax.fill(0);
+	initAddr();
 	parse();
 }
 
 IPcalc::~IPcalc()
 {
+}
+
+std::string IPcalc::getLongNetmask()
+{
+	return addrToString(m_longNetmask);
+}
+
+std::string IPcalc::getNetwork()
+{
+	return addrToString(m_network);
+}
+
+void IPcalc::setAddress(const std::string &addr)
+{
+	m_addressRaw = addr;
+	initAddr();
+	parse();
 }
 
 void IPcalc::parse()
@@ -50,11 +62,32 @@ void IPcalc::parse()
 	int power = 7;
 	while (bitCount > 0)
 	{
-		m_longNetmask.at(byteIndex) += pow(2, power);
+		m_longNetmask.at(byteIndex) += 1 << power;
 		bitCount--;
 		power--;
 	}
-	/*std::cout << bitCount << std::endl;
-	for (int i = 0; i < 4; i++)
-		std::cout << (int)m_longNetmask.at(i) << std::endl;*/
+	//
+	//Generates the network address m_network
+	//
+	for (int i = 0; i < m_address.size(); i++)
+		m_network.at(i) = m_address.at(i) & m_longNetmask.at(i);
+}
+
+std::string IPcalc::addrToString(const std::array<uint8_t, 4> &addr)
+{
+	std::string strAddr;
+	int i;
+	for (i = 0; i < addr.size() - 1; i++)
+		strAddr += std::to_string(addr.at(i)) + '.';
+	strAddr += std::to_string(addr.at(i));
+	return strAddr;
+}
+
+void IPcalc::initAddr()
+{
+	m_longNetmask.fill(0);
+	m_network.fill(0);
+	m_broadcast.fill(0);
+	m_hostMin.fill(0);
+	m_hostMax.fill(0);
 }
--- IPcalc/IPcalc.h
+++ IPcalc/IPcalc.h
@@ -6,10 +6,16 @@
 class IPcalc
 {
 public:
-	IPcalc(std::string);
+	IPcalc(std::string = "192.168.0.1/24");
 	~IPcalc();
+	std::string getLongNetmask();
+	std::string getNetwork();
+	void setAddress(const std::string &);
 private:
 	void parse();
+	std::string addrToString(const std::array<uint8_t, 4> &);
+	void initAddr();
+
 	std::array<uint8_t, 4> m_address, m_longNetmask, m_network, m_broadcast, m_hostMin, m_hostMax;
 	uint8_t m_netmask;
 	std::string m_addressRaw;
